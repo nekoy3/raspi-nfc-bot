a6b0b43d894ae97b97b5ab658e930ec92368532b
Haruki KOGA <113566745+k21rs044@users.noreply.github.com>
2022-10-06 18:24:58 +0900

    データベース閲覧用コードを追加

---
+++ tests/test_db.py
@@ -0,0 +1,17 @@
+import sys 
+import sqlite3
+
+def main(args):
+    conn = sqlite3.connect("student_list.db")
+    cur = conn.cursor()
+    if args[1] == "get":
+        cur.execute("SELECT * FROM student")
+        exist = cur.fetchall()
+        print(str(exist))
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    main(args)
